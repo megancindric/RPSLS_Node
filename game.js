eea3fbd4c9d032e8457a0bf86336abee6e67077e
Megan Cindric <megancindric@gmail.com>
2021-11-01 11:30:38 -0500

    User interface methods

--- game.js
+++ game.js
@@ -7,16 +7,42 @@ class Game {
     }
 
     displayWelcomeRules(){
-
+        console.log("Welcome to Rock-Paper-Scissors-Lizard-Spock!  Here be the rules:")
+        console.log(`
+        Rock crushes Scissors
+        Scissors cuts Paper
+        Paper covers Rock
+        Rock crushes Lizard
+        Lizard poisons Spock
+        Spock smashes Scissors
+        Scissors decapitates Lizard
+        Lizard eats Paper
+        Paper dispoves Spock
+        Spock vaporizes Rock
+        `)
     }
 
     selectGameMode(){
+        let gameMode = prompt("Press 1 for PvP.  Press 2 for PvAI. ");
+        if(gameMode == 1){
 
+        }
+        else if(gameMode == 2){
+
+        }
+        else{
+            console.log("Invalid choice - try again!")
+            this.selectGameMode();
+        }
     }
     playBestOfThree(){
         //While p1 & p2 scores are below 3...
     }
     singleRound(){
+
+    }
+
+    runGame(){
 
     }
 
@@ -29,42 +55,42 @@ class Game {
         }
         else if(this.playerOne.selectedGesture == "rock"){
             if(this.playerTwo.selectedGesture == "scissors" || this.playerTwo.selectedGesture == "lizard"){
-                displayRoundResults(this.playerOne, this.playerTwo);
+                this.displayRoundResults(this.playerOne, this.playerTwo);
             }
             else{
-                displayRoundResults(this.playerTwo, this.playerOne);
+                this.displayRoundResults(this.playerTwo, this.playerOne);
             }
         }
         else if(this.playerOne.selectedGesture == "paper"){
             if(this.playerTwo.selectedGesture == "rock" || this.playerTwo.selectedGesture == "spock"){
-                displayRoundResults(this.playerOne, this.playerTwo);
+                this.displayRoundResults(this.playerOne, this.playerTwo);
             }
             else{
-                displayRoundResults(this.playerTwo, this.playerOne);
+                this.displayRoundResults(this.playerTwo, this.playerOne);
             }
         }
         else if(this.playerOne.selectedGesture == "scissors"){
             if(this.playerTwo.selectedGesture == "paper" || this.playerTwo.selectedGesture == "lizard"){
-                displayRoundResults(this.playerOne, this.playerTwo);
+                this.displayRoundResults(this.playerOne, this.playerTwo);
             }
             else{
-                displayRoundResults(this.playerTwo, this.playerOne);
+                this.displayRoundResults(this.playerTwo, this.playerOne);
             }
         }
         else if(this.playerOne.selectedGesture == "lizard"){
             if(this.playerTwo.selectedGesture == "spock" || this.playerTwo.selectedGesture == "paper"){
-                displayRoundResults(this.playerOne, this.playerTwo);
+                this.displayRoundResults(this.playerOne, this.playerTwo);
             }
             else{
-                displayRoundResults(this.playerTwo, this.playerOne);
+                this.displayRoundResults(this.playerTwo, this.playerOne);
             }
         }
         else if(this.playerOne.selectedGesture == "spock"){
             if(this.playerTwo.selectedGesture == "rock" || this.playerTwo.selectedGesture == "scisors"){
-                displayRoundResults(this.playerOne, this.playerTwo);
+                this.displayRoundResults(this.playerOne, this.playerTwo);
             }
             else{
-                displayRoundResults(this.playerTwo, this.playerOne);
+                this.displayRoundResults(this.playerTwo, this.playerOne);
             }
         }
     }
@@ -73,5 +99,29 @@ class Game {
         console.log(`${winner.selectedGesture} beats ${loser.selectedGesture}`);
         console.log(`This round goes to ${winner.name}`);    
         winner.score++;
+    }
+
+    displayGameWinner(){
+        if(this.playerOne.score > this.playerTwo.score){
+            console.log(`This game's winner is: ${this.playerOne.name}!`);
+        }
+        else{
+            console.log(`This game's winner is: ${this.playerTwo.name}!`);
+        }
+    }
+
+    checkForReplay(){
+        let userInput = prompt("Press 1 to play again, press 2 to exit!")
+
+        if(userInput == 1){
+            this.runGame()
+        }
+        else if (userInput == 2){
+            break;
+        }
+        else{
+            console.log("Invalid input - try again!");
+            this.checkForReplay();
+        }
     }
 }
